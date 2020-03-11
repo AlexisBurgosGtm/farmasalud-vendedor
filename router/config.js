b35a059ec2394efeeee241ad21dd86dd5c5e7503
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-03-11 10:56:08 -0600

    cambio hos

--- router/config.js
+++ router/config.js
@@ -3,4 +3,6 @@ const config = {user: 'DB_A54053_Respaldobd_admin',password: 'Alexis2020',server
 //const config = {user: 'iEx', password: 'iEx', server: 'SERVERALEXIS\\SQLEXPRESS', database: 'DB_A45479_EXPRESS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
 
+
+
 module.exports = config;
