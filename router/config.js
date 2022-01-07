5e63d42bd37d40e50af4543061cd351914330b1f
Alexis Burgos (gtm) <alexisburgosgtm@outlook.com>
2022-01-07 15:04:37 -0600

    logos

--- router/config.js
+++ router/config.js
@@ -1,8 +1,8 @@
 const config = {
-    user: 'DB_A54053_Respaldobd_admin',
-    password: 'Alexis2020',
-    server: 'SQL5049.site4now.net',
-    database: 'DB_A54053_Respaldobd',
+    user: 'DB_A671FA_farmasalud_admin',
+    password: 'Celta2005',
+    server: 'sql5095.site4now.net',
+    database: 'DB_A671FA_farmasalud',
     pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}
 };
 
